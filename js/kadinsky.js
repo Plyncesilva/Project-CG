45c86db25dbf40b220ec8b7ad5f4552b965f60f2
Plyncesilva <91795134+Plyncesilva@users.noreply.github.com>
2022-05-16 19:21:10 +0100

    Horizontal and Vertical movement implementation

--- js/kadinsky.js
+++ js/kadinsky.js
@@ -12,6 +12,10 @@ var rotateBallLeft = false, rotateBallRight = false;
 var rotateCubeLeft = false, rotateCubeRight = false;
 var rotateRectangleLeft = false, rotateRectangleRight = false;
 
+var moveUp = false, moveDown = false;
+var moveRight = false, moveLeft = false;
+var moveUpUp = false, moveDownDown = false;
+
 var kadinsky, kadinskySec, kadinskyTer, kadinskyNotMove;
 
 
@@ -160,23 +164,42 @@ function checkRotate() {
     var delta = clock.getDelta();
 
     if(rotateBallLeft)
-        kadinsky.rotateY(delta);
+        kadinsky.rotateY(-delta);
 
     if(rotateBallRight)
-        kadinsky.rotateY(-delta); 
+        kadinsky.rotateY(delta); 
     
     if(rotateCubeLeft)
-        kadinskySec.rotateX(delta);
-
-    if(rotateCubeRight)
         kadinskySec.rotateX(-delta);
 
+    if(rotateCubeRight)
+        kadinskySec.rotateX(delta);
+
     if(rotateRectangleLeft)
-        kadinskyTer.rotateX(delta);
+        kadinskyTer.rotateX(-delta);
 
     if(rotateRectangleRight)
-        kadinskyTer.rotateX(-delta);
+        kadinskyTer.rotateX(delta);
     
+}
+
+function checkMovement() {
+    'use strict';
+    // var delta = clock.getDelta();
+
+    if (moveUp)
+        kadinsky.position.z += -0.5;
+    if (moveDown)
+        kadinsky.position.z += 0.5;
+    if (moveRight)
+        kadinsky.position.x += 0.5;
+    if (moveLeft)
+        kadinsky.position.x += -0.5;
+    if (moveUpUp)
+        kadinsky.position.y += 0.5;
+    if (moveDownDown)
+        kadinsky.position.y += -0.5;
+
 }
 
 function onResize() {
@@ -198,95 +221,115 @@ function onKeyDown(e) {
     'use strict';
     
     switch (e.keyCode) {
-    case 49: //1
-        camera=camera1;
-        break;
-    case 50: //2
-        camera=camera2;
-        break;
-    case 51: //3
-        camera=camera3;
-        break;
-    case 52: //4
-        scene.traverse(function (node) {
-            if (node instanceof THREE.Mesh) {
-                node.material.wireframe = !node.material.wireframe;
-            }
-        });
-        render();
-        break;
-  
-    //controlar  angulo θ1 que roda todo o objecto
-    case 81: //Q
-    case 113: //q
-        if (rotateBallRight)
-            rotateBallRight = false;
-        rotateBallLeft = !rotateBallLeft;
-        break;
+        case 38: // Arrow Up
+            moveUp = true;
+            break;
+        case 40: // Arrow Down
+            moveDown = true;
+            break;
+        case 39: // Arrow Right
+            moveRight = true;
+            break;
+        case 37: // Arrow Left
+            moveLeft = true;
+            break;
+        case 68: // D UpUp
+        case 100: // d UpUp   
+            moveUpUp = true;
+            break;
+        case 67: // C DownDown
+        case 99: // c DownDown
+            moveDownDown = true;
+            break;
+        case 49: //1
+            camera=camera1;
+            break;
+        case 50: //2
+            camera=camera2;
+            break;
+        case 51: //3
+            camera=camera3;
+            break;
+        case 52: //4
+            scene.traverse(function (node) {
+                if (node instanceof THREE.Mesh) {
+                    node.material.wireframe = !node.material.wireframe;
+                }
+            });
+            render();
+            break;
     
-    case 87: //W
-    case 119: //w
-        if (rotateBallLeft)
-            rotateBallLeft = false;
-        rotateBallRight = !rotateBallRight;
-        break;
-
-    
-    // controlar o angulo θ2 de um ramo secundario
-    case 65: //A
-    case 97: //a
-        if (rotateCubeRight)
-            rotateCubeRight = false;
-        rotateCubeLeft = !rotateCubeLeft;
-        break;
-    case 83:  //S
-    case 115: //s
-        if (rotateCubeLeft)
-            rotateCubeLeft = false;
-        rotateCubeRight = !rotateCubeRight;
-        break;
-
-    //controlar o ângulo θ3 de um ramo terciario.
-    case 90: // Z
-    case 122: //z
-        if(rotateRectangleRight)
-            rotateRectangleRight = false;
-        rotateRectangleLeft = !rotateRectangleLeft;
-        break;
-
-    case 88: //X
-    case 120: //x
-        if (rotateRectangleLeft)
-            rotateRectangleLeft = false;
-        rotateRectangleRight = !rotateRectangleRight;
-        break;
+        //controlar  angulo θ1 que roda todo o objecto
+        case 81: //Q
+        case 113: //q
+            if (rotateBallRight)
+                rotateBallRight = false;
+            rotateBallLeft = !rotateBallLeft;
+            break;
+        
+        case 87: //W
+        case 119: //w
+            if (rotateBallLeft)
+                rotateBallLeft = false;
+            rotateBallRight = !rotateBallRight;
+            break;
 
         
+        // controlar o angulo θ2 de um ramo secundario
+        case 65: //A
+        case 97: //a
+            if (rotateCubeRight)
+                rotateCubeRight = false;
+            rotateCubeLeft = !rotateCubeLeft;
+            break;
+        case 83:  //S
+        case 115: //s
+            if (rotateCubeLeft)
+                rotateCubeLeft = false;
+            rotateCubeRight = !rotateCubeRight;
+            break;
+
+        //controlar o ângulo θ3 de um ramo terciario.
+        case 90: // Z
+        case 122: //z
+            if(rotateRectangleRight)
+                rotateRectangleRight = false;
+            rotateRectangleLeft = !rotateRectangleLeft;
+            break;
+
+        case 88: //X
+        case 120: //x
+            if (rotateRectangleLeft)
+                rotateRectangleLeft = false;
+            rotateRectangleRight = !rotateRectangleRight;
+            break;
+
+            
 
 
-    case 68: //D
-    case 100: //d
-        /*scene.remove(kadinsky);
-        createKadinskyTer(0,0,0);
-        createKadinskySec(0,0,0);
-        createKadinsky(0, 0, 0);*/
-        break;
+        case 68: //D
+        case 100: //d
+            /*scene.remove(kadinsky);
+            createKadinskyTer(0,0,0);
+            createKadinskySec(0,0,0);
+            createKadinsky(0, 0, 0);*/
+            break;
 
-    case 67: //C
-    case 99: //c
-        break;
+        case 67: //C
+        case 99: //c
+            break;
 
-    
-    
-    
-    case 69:  //E
-    case 101: //e
-        scene.traverse(function (node) {
-            if (node instanceof THREE.AxisHelper) {
-                node.visible = !node.visible;
-            }
-        });
-        break;
+        
+        
+        
+        case 69:  //E
+        case 101: //e
+            scene.traverse(function (node) {
+                if (node instanceof THREE.AxisHelper) {
+                    node.visible = !node.visible;
+                }
+            });
+            break;
     }
 
     
@@ -297,37 +340,57 @@ function onKeyUp(e) {
     'use strict';
         
     switch (e.keyCode) {
-    //parar a rotação do  angulo θ1 que roda todo o objecto
-    case 81: //Q
-    case 113: //q
-        rotateBallLeft=false;
-        break;
+        case 38: // Arrow Up
+            moveUp = false;
+            break;
+        case 40: // Arrow Down
+            moveDown = false;
+            break;
+        case 39: // Arrow Right
+            moveRight = false;
+            break;
+        case 37: // Arrow Left
+            moveLeft = false;
+            break;
+        case 68: // d UpUp
+        case 100: // D UpUp   
+            moveUpUp = false;
+            break;
+        case 67: // C DownDown
+        case 99: // c DownDown
+            moveDownDown = false;
+            break;
+        // //parar a rotação do  angulo θ1 que roda todo o objecto
+        // case 81: //Q
+        // case 113: //q
+        //     rotateBallLeft=false;
+        //     break;
+        
+        // case 87: //W
+        // case 119: //w
+        //     rotateBallRight=false;
+        //     break;
+
+        // // parar a rotação do angulo θ2 de um ramo secundario
+        // case 65: //A
+        // case 97: //a
+        //     rotateCubeLeft = false;
+        //     break;
+        // case 83:  //S
+        // case 115: //s
+        //     rotateCubeRight = false;
+        //     break;
+
+        // //controlar o ângulo θ3 de um ramo terciario.
+        // case 90: // Z
+        // case 122: //z
+        //     rotateRectangleLeft = false;
+        //     break;
     
-    case 87: //W
-    case 119: //w
-        rotateBallRight=false;
-        break;
-
-    // parar a rotação do angulo θ2 de um ramo secundario
-    case 65: //A
-    case 97: //a
-        rotateCubeLeft = false;
-        break;
-    case 83:  //S
-    case 115: //s
-        rotateCubeRight = false;
-        break;
-
-     //controlar o ângulo θ3 de um ramo terciario.
-     case 90: // Z
-     case 122: //z
-         rotateRectangleLeft = false;
-         break;
- 
-     case 88: //X
-     case 120: //x
-         rotateRectangleRight = false;
-         break;
+        // case 88: //X
+        // case 120: //x
+        //     rotateRectangleRight = false;
+        //     break;
 
     }
 
@@ -355,20 +418,23 @@ function init() {
     render();
     
     window.addEventListener("keydown", onKeyDown);
-    // window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("keyup", onKeyUp);
     window.addEventListener("resize", onResize);
 }
 
 function animate() {
     'use strict';
 
+    // Update
     checkRotate();
+    checkMovement();
     /*
     if (ball.userData.jumping) {
         ball.userData.step += 0.04;
         ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
         ball.position.z = 15 * (Math.cos(ball.userData.step));
     }*/
+    // Display
     render();
     
     requestAnimationFrame(animate);
