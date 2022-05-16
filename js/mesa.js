207e5c45597b27584e8875a5ae8f35a8c248dce0
JoaoJose10 <jjmyst10@gmail.com>
2022-05-16 12:37:36 +0100

    Rotate ball

--- js/mesa.js
+++ js/mesa.js
@@ -2,8 +2,7 @@
 
 const { OrthographicCamera } = require("./three");
 
-var camera, scene, renderer;
-
+var camera, camera1, camera2, camera3, camera0, scene, renderer;
 var geometry, material, mesh;
 
 var sphere_material, cube_material, rectangle_material;
@@ -11,42 +10,10 @@ var sphere_material, cube_material, rectangle_material;
 var ball;
 var rotateBallLeft = false, rotateBallRight = false;
 
-var kadinsky;
+var kadinsky, kadinskySec, kadinskyTer;
+
 
 var clock = new THREE.Clock();
-
-function addTableLeg(obj, x, y, z) {
-    'use strict';
-                                    //x (vermelho) , z(verde), y(azul) 
-    geometry = new THREE.CubeGeometry(0, 5, 5);
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y - 3, z);
-    obj.add(mesh);
-}
-
-function addTableTop(obj, x, y, z) {
-    'use strict';
-    geometry = new THREE.CubeGeometry(60, 2, 20);
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
-    obj.add(mesh);
-}
-
-function createBall(x, y, z) {
-    'use strict';
-    
-    ball = new THREE.Object3D();
-    //ball.userData = { jumping: true, step: 0 };
-    
-    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-    geometry = new THREE.SphereGeometry(5, 0, 0);
-    mesh = new THREE.Mesh(geometry, material);
-    
-    ball.add(mesh);
-    ball.position.set(x, y, z);
-    
-    scene.add(ball);
-}
 
 
 function addSphere(obj, x, y, z) {
@@ -58,7 +25,7 @@ function addSphere(obj, x, y, z) {
     //ball.userData = { jumping: true, step: 0 };
     
     // material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-    geometry = new THREE.SphereGeometry(5, 10, 10);
+    geometry = new THREE.SphereGeometry(2.5, 10, 10);
     mesh = new THREE.Mesh(geometry, sphere_material);
     
     // ball.add(mesh);
@@ -102,19 +69,46 @@ function addPyramid(obj, x, y, z) {
     obj.add(mesh);
 }
 
+function createKadinskyTer(x, y, z){
+    'use strict';
+
+    kadinskyTer= new THREE.Object3D();
+    addRectangle(kadinskyTer, -1.5, -3, 1.5);
+    //scene.add(kadinskyTer);    
+
+}
+
+
+function createKadinskySec(x, y, z){
+    'use strict';
+
+    kadinskySec= new THREE.Object3D();
+    addCube(kadinskySec, -2.5, -2.5, 2.5);
+    kadinskyTer.position.set(-5,-5,5);
+    kadinskySec.add(kadinskyTer);
+
+    //scene.add(kadinskySec);    
+
+}
+
 function createKadinsky(x, y, z){
     'use strict';
 
     kadinsky = new THREE.Object3D();
     
     sphere_material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-    cube_material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-    rectangle_material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+    //cube_material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+    //rectangle_material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
    
-    addSphere(kadinsky, 15, 19.625, -2.5);
-    addCube(kadinsky, 10, 15, 2.5);
+    //addSphere(kadinsky, 15, 19.625, -2.5);
+    //addCube(kadinsky, 10, 15, 2.5);
     // addPyramid(kadinsky, 7.5, 11.2, 5);
-    addRectangle(kadinsky, 6, 9.5, 6.5);    
+    //addRectangle(kadinsky, 6, 9.5, 6.5);    
+    addSphere(kadinsky, 0, 0, 0);
+    //addCube(kadinsky)
+
+    kadinskySec.position.set(0,0,2.5);
+    kadinsky.add(kadinskySec);
 
     scene.add(kadinsky);
     
@@ -124,26 +118,6 @@ function createKadinsky(x, y, z){
 
 }
 
-function createTable(x, y, z) {
-    'use strict';
-    
-    var table = new THREE.Object3D();
-    
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-   
-    addTableTop(table, 0, 0, 0);
-    addTableLeg(table, -25, -1, -8);
-    addTableLeg(table, -25, -1, 8);
-    addTableLeg(table, 25, -1, 8);
-    addTableLeg(table, 25, -1, -8);
-    
-    scene.add(table);
-    
-    table.position.x = x;
-    table.position.y = y;
-    table.position.z = z;
-}
-
 function createScene() {
     'use strict';
     
@@ -151,7 +125,9 @@ function createScene() {
     
 
     scene.add(new THREE.AxisHelper(10));
-    
+    createKadinskyTer(0,0,0);
+    createKadinskySec(0,0,0);
+
     createKadinsky(0, 0, 0);
     //15, 19.625, -2.5
     //createKadinsky(15, 19.625, -2.5);
@@ -170,19 +146,45 @@ function createCamera() {
     camera.lookAt(scene.position);
 }
 
-function createCamera_xy() {
+function createCameras() {
     'use strict';
-    camera = new THREE.OrthographicCamera( -10, 40, 50, -20, 1, 1000 );
-    // camera = new THREE.PerspectiveCamera(70,
-    //                                      window.innerWidth / window.innerHeight,
-    //                                      1,
-    //                                      1000);
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 100;
-    camera.lookAt(scene.position);
-}
 
+    camera0 = new THREE.PerspectiveCamera(70,
+                                          window.innerWidth / window.innerHeight,
+                                              1,
+                                              1000);
+
+                                              camera0.position.x = 0;
+                                              camera0.position.y = 100;
+                                              camera0.position.z = 0;
+                                              camera0.lookAt(scene.position);
+    camera1 = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2 );
+    camera2 =new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2 );
+    camera3 = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2 );
+
+    camera1.position.x = 50;
+    camera1.position.y = 50;
+    camera1.position.z = 50;
+    camera1.lookAt(scene.position);
+
+  
+    camera2.position.x = 0;
+    camera2.position.y = 0;
+    camera2.position.z = 100;
+    camera2.lookAt(scene.position);
+
+    camera3.position.x = 0;
+    camera3.position.y = 100;
+    camera3.position.z = 0;
+    camera3.lookAt(scene.position);
+    
+    scene.add(camera0);
+    scene.add(camera1);
+
+
+    camera = camera0;
+}
+/*
 function createCamera_xz() {
     'use strict';
     camera = new THREE.OrthographicCamera( -10, 40, 20, -20, 1, 1000 );
@@ -194,16 +196,17 @@ function createCamera_xz() {
     camera.position.y = 100;
     camera.position.z = 0;
     camera.lookAt(scene.position);
-}
+}*/
 
 
 function checkRotate() {
     'use strict';
 
     if(rotateBallLeft){
+        var delta = clock;
         // 15, 19.625, -2.5);
-        kadinsky.position.set(-15,-19.625,+2.5);
-        kadinsky.rotateX(1);
+        //kadinsky.position.set(-15,-19.625,+2.5);
+        kadinsky.rotateY(1);
         
 
     }
@@ -230,16 +233,13 @@ function onKeyDown(e) {
     
     switch (e.keyCode) {
     case 49: //1
-        createCamera();
-        render();
+        camera=camera1;
         break;
     case 50: //2
-        createCamera_xy();
-        render();
+        camera=camera2;
         break;
     case 51: //3
-        createCamera_xz();
-        render();
+        camera=camera3;
         break;
     case 52: //4
         scene.traverse(function (node) {
@@ -333,7 +333,7 @@ function init() {
     document.body.appendChild(renderer.domElement);
    
     createScene();
-    createCamera();
+    createCameras();
     
     render();
     
